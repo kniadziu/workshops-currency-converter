d0e179c5ebba9bbaaaf0fd83bd476f7526b31ac1
patrykomiotek <patryk.omiotek@gmail.com>
2020-09-12 12:21:25 +0200

    calculator with events

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,11 +4,19 @@ function Button(props) { // function Button({ label, color })
   // const { label } = props;
   return (
     // <button>{props.label}</button>
-    <button>{props.children}</button>
+    <button type={props.type}>{props.children}</button>
   );
 }
 Button.defaultProps = {
-  label: 'Enter label for button'
+  label: 'Enter label for button',
+  type: 'submit'
 }
 
 export default Button;
+
+// App.js
+{/* <div>
+  <span>Tekst</span>
+  <Button type="button">Cokolwiek</Button>
+  <Button>Cokolwiek 2</Button>
+</div> */}
