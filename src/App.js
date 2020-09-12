d1dce8483144bad08910c616f8ea8bbb43c6c800
patrykomiotek <patryk.omiotek@gmail.com>
2020-09-12 10:52:10 +0200

    button component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const day = 'Saturday';
+
+function Button() {
+  return (
+    <button>Click me!!!</button>
+  );
+}
+
 function App() {
+  // JSX
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello!!</p>
+      <p>{day}</p>
+      <Button />
+      <Button />
+      <Button />
     </div>
   );
 }
